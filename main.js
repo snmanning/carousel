a783441c6a4d5f7a2f6586fbfa65b9b41b990d28
Sarah Manning <sarahnmanning@gmail.com>
2018-06-29 20:59:45 -0400

    hammered out the main structures and created all the necessary files.

---
+++ main.js
@@ -0,0 +1,24 @@
+class Carousel {
+    constructor() {
+        this.XXXX = YYYY
+        this.cacheDom();
+        this.addEventListeners();
+        this.render();
+    }
+
+    cacheDom() {
+        this.root = document.querySelector('#spinner');
+        
+    }
+
+    bindEventListeners() {
+        this.
+    }
+
+    render() {
+        this.
+    }
+
+}
+
+window.XXXX = new Carousel[];
